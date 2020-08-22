fed324358de42b7ecbbfb173928aa8ae00fc0678
Michael da Costa Móra <michael.c.mora@gmail.com>
2020-08-22 00:22:57 -0300

    Finalizado, com casos de teste.

--- src/test/java/br/pucrs/exercontagem/ParImparTest.java
+++ src/test/java/br/pucrs/exercontagem/ParImparTest.java
@@ -27,24 +27,20 @@ class ParImparTest {
 	void parImparInPlace1Test() {
 		int [] aux1 = {1, 2, 3, 4, 5, 6};
 		int [] res = {6, 2, 4, 3, 5, 1};
-
+		assertArrayEquals(res, pi.sepParImparInPlace1(aux1) );
 	}
 	
 	@Test
 	void parImparAux2Test() {
 		int [] aux2 = {1, 3, 5, 2, 4, 6};
 		int [] res = {2, 4, 6, 1, 3, 5};
-		
-		
-		
+		assertArrayEquals(res, pi.sepParImparAux1(aux2) );
 	}
 	
 	@Test
 	void parImparInPlace2Test() {
 		int [] aux2 = {1, 3, 5, 2, 4, 6};
 		int [] res = {6, 4, 2, 5, 3, 1};
-		
-		
-		
+		assertArrayEquals(res, pi.sepParImparInPlace1(aux2) );
 	}
 }
